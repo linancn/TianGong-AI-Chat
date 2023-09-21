67c07febb51f4ef0592feb4c388770839e7e33a5
jing <jingguo92@gmail.com>
2023-09-21 18:23:05 +0800

    Refactoring

--- Chat.py
+++ Chat.py
@@ -5,22 +5,14 @@ import re
 import time
 from datetime import datetime
 
-import pinecone
 import streamlit as st
 from langchain import LLMChain, PromptTemplate
-from langchain.chains.openai_functions import create_structured_output_chain
 from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
-from langchain.schema import HumanMessage, SystemMessage
-from langchain.tools import DuckDuckGoSearchResults, WikipediaQueryRun
-from langchain.utilities import WikipediaAPIWrapper
-from langchain.vectorstores import Pinecone
 
 import ui_config
 import utils
 from streaming import StreamHandler
-from utils import xata_chat_history
+from utils import search_pinecone, search_internet, seach_docs, func_calling_chain, chat_history_chain, main_chain, xata_chat_history
 
 llm_model = st.secrets["llm_model"]
 langchain_verbose = st.secrets["langchain_verbose"]
@@ -90,7 +82,7 @@ if auth:
         st.subheader(ui.sidebar_subheader)
 
         with st.expander(ui.sidebar_expander_title, expanded=False):
-            search_internet = st.toggle(ui.search_internet_checkbox_label, value=False)
+            search_online = st.toggle(ui.search_internet_checkbox_label, value=False)
             search_docs = st.toggle(ui.search_docs_checkbox_label, value=False)
 
             if search_docs:
@@ -194,174 +186,8 @@ if auth:
             self.openai_model = llm_model
             self.verbose = langchain_verbose
 
-        def search_pinecone(self, query, created_at, top_k=16):
-            embeddings = OpenAIEmbeddings()
-            pinecone.init(
-                api_key=st.secrets["pinecone_api_key"],
-                environment=st.secrets["pinecone_environment"],
-            )
-            vectorstore = Pinecone.from_existing_index(
-                index_name=st.secrets["pinecone_index"],
-                embedding=embeddings,
-            )
-            if created_at is not None:
-                docs = vectorstore.similarity_search(
-                    query, k=top_k, filter={"created_at": created_at}
-                )
-            else:
-                docs = vectorstore.similarity_search(query, k=top_k)
-
-            docs_list = []
-            for doc in docs:
-                date = datetime.fromtimestamp(doc.metadata["created_at"])
-                formatted_date = date.strftime("%Y-%m")  # Format date as 'YYYY-MM'
-                source_entry = "[{}. {}. {}.]({})".format(
-                    doc.metadata["source_id"],
-                    doc.metadata["author"],
-                    formatted_date,
-                    doc.metadata["url"],
-                )
-                docs_list.append({"content": doc.page_content, "source": source_entry})
-
-            return docs_list
-
-        def search_internet(self, query):
-            search = DuckDuckGoSearchResults()
-            results = search.run(query)
-
-            pattern = r"\[snippet: (.*?), title: (.*?), link: (.*?)\]"
-            matches = re.findall(pattern, results)
-
-            docs = [
-                {"snippet": match[0], "title": match[1], "link": match[2]}
-                for match in matches
-            ]
-
-            docs_list = []
-
-            for doc in docs:
-                docs_list.append(
-                    {
-                        "content": doc["snippet"],
-                        "source": "[{}]({})".format(doc["title"], doc["link"]),
-                    }
-                )
-
-            return docs_list
-
-        def search_wiki(self, query):
-            wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-            response_wiki = wikipedia.run(query)
-
-            return response_wiki
-
-        def seach_docs(self, query, top_k=16):
-            docs = st.session_state["faiss_db"].similarity_search(query, k=top_k)
-            docs_list = []
-            for doc in docs:
-                source_entry = doc.metadata["source"]
-                docs_list.append({"content": doc.page_content, "source": source_entry})
-
-            return docs_list
-
-        def func_calling_chain(self):
-            func_calling_json_schema = {
-                "title": "get_querys_and_filters_to_search_embeddings",
-                "description": "Extract the next queries and filters for a vector database semantic search from a chat history.",
-                "type": "object",
-                "properties": {
-                    "query": {
-                        "title": "Query",
-                        "description": "The next queries extracted for a vector database semantic search from a chat history in the format of a JSON object",
-                        "type": "string",
-                    },
-                    "created_at": {
-                        "title": "Date Filters",
-                        "description": 'Date extracted for a vector database semantic search from a chat history, in MongoDB\'s query and projection operators, in format like {"$gte": 1609459200.0, "$lte": 1640908800.0}',
-                        "type": "string",
-                    },
-                    # "author": {
-                    #     "title": "Author Filters",
-                    #     "description": 'Author(s) extracted in MongoDB\'s query and projection operators, in format like {"$in": ["John Doe"]}',
-                    #     "type": "string",
-                    # },
-                },
-                "required": ["query"],
-            }
-
-            prompt_func_calling_msgs = [
-                SystemMessage(
-                    content="You are a world class algorithm for extracting the next queries and filters for a vector database semantic search from a chat history. Make sure to answer in the correct structured format"
-                ),
-                HumanMessage(content="The chat history:"),
-                HumanMessagePromptTemplate.from_template("{input}"),
-            ]
-
-            prompt_func_calling = ChatPromptTemplate(messages=prompt_func_calling_msgs)
-
-            llm_func_calling = ChatOpenAI(
-                model_name=self.openai_model, temperature=0, streaming=False
-            )
-
-            func_calling_chain = create_structured_output_chain(
-                output_schema=func_calling_json_schema,
-                llm=llm_func_calling,
-                prompt=prompt_func_calling,
-                verbose=self.verbose,
-            )
-
-            return func_calling_chain
-
-        def chat_history_chain(self):
-            llm_chat_history = ChatOpenAI(
-                model=self.openai_model,
-                temperature=0,
-                streaming=False,
-                verbose=self.verbose,
-            )
-
-            template = """Return highly concise and well-organized chat history from: {input}"""
-            prompt = PromptTemplate(
-                input_variables=["input"],
-                template=template,
-            )
-
-            chat_history_chain = LLMChain(
-                llm=llm_chat_history,
-                prompt=prompt,
-                verbose=self.verbose,
-            )
-
-            return chat_history_chain
-
-        def main_chain(self):
-            llm_chat = ChatOpenAI(
-                model=self.openai_model,
-                temperature=0,
-                streaming=True,
-                verbose=self.verbose,
-            )
-
-            template = """You MUST ONLY responese to science-related quests.
-            DO NOT return any information on politics, ethnicity, gender, national sovereignty, or other sensitive topics.
-            {input}
-            Use bullet points if a better expression effect can be achieved."""
-
-            prompt = PromptTemplate(
-                input_variables=["input"],
-                template=template,
-            )
-
-            chain = LLMChain(
-                llm=llm_chat,
-                prompt=prompt,
-                verbose=self.verbose,
-            )
-
-            return chain
-
         @utils.enable_chat_history
-        def main(self):
+        def main():
             # if "chat_history" not in st.session_state:
             #     st.session_state["chat_history"] = ChatMessageHistory()
             # else:
@@ -375,7 +201,7 @@ if auth:
                 st.session_state["xata_history"].add_user_message(user_query)
 
                 if len(st.session_state["xata_history"].messages) <= 1:
-                    func_calling_response = self.func_calling_chain().run(user_query)
+                    func_calling_response = func_calling_chain().run(user_query)
 
                     query = func_calling_response.get("query")
                     try:
@@ -387,45 +213,45 @@ if auth:
 
                     if search_docs:
                         if search_docs_option == ui.search_docs_options_isolated:
-                            docs_response = self.seach_docs(query, top_k=16)
+                            docs_response = seach_docs(query, top_k=16)
                             input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{docs_response}". Do not return any prefix like "AI:". Give corresponding detailed sources."""
                         elif search_docs_option == ui.search_docs_options_combinedd:
-                            if search_internet:
-                                embedding_results = self.search_pinecone(
+                            if search_online:
+                                embedding_results = search_pinecone(
                                     query, created_at, top_k=8
                                 )
-                                docs_response = self.seach_docs(query, top_k=8)
+                                docs_response = seach_docs(query, top_k=8)
                                 docs_response.extend(embedding_results)
-                                internet_results = self.search_internet(query)
+                                internet_results = search_internet(query)
                                 docs_response.extend(internet_results)
                                 input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{docs_response}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls."""
-                            elif not search_internet:
-                                embedding_results = self.search_pinecone(
+                            elif not search_online:
+                                embedding_results = search_pinecone(
                                     query, created_at, top_k=8
                                 )
-                                docs_response = self.seach_docs(query, top_k=8)
+                                docs_response = seach_docs(query, top_k=8)
                                 docs_response.extend(embedding_results)
                                 input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{docs_response}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls."""
                     elif not search_docs:
-                        if search_internet:
-                            embedding_results = self.search_pinecone(
+                        if search_online:
+                            embedding_results = search_pinecone(
                                 query, created_at, top_k=16
                             )
-                            internet_results = self.search_internet(query)
+                            internet_results = search_internet(query)
                             embedding_results.extend(internet_results)
                             input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{embedding_results}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls."""
-                        elif not search_internet:
-                            embedding_results = self.search_pinecone(
+                        elif not search_online:
+                            embedding_results = search_pinecone(
                                 query, created_at, top_k=16
                             )
                             input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{embedding_results}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls."""
                 else:
-                    chat_history_response = self.chat_history_chain()(
+                    chat_history_response = chat_history_chain()(
                         {"input": st.session_state["xata_history"].messages[-7:]},
                     )
                     chat_history_summary = chat_history_response["text"]
 
-                    func_calling_response = self.func_calling_chain().run(
+                    func_calling_response = func_calling_chain().run(
                         chat_history_summary
                     )
 
@@ -440,42 +266,42 @@ if auth:
 
                     if search_docs:
                         if search_docs_option == ui.search_docs_options_isolated:
-                            docs_response = self.seach_docs(query, top_k=16)
+                            docs_response = seach_docs(query, top_k=16)
                             input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{docs_response}". Do not return any prefix like "AI:". Give corresponding detailed sources. Current conversation:"{chat_history_summary}"""
                         elif search_docs_option == ui.search_docs_options_combined:
-                            if search_internet:
-                                embedding_results = self.search_pinecone(
+                            if search_online:
+                                embedding_results = search_pinecone(
                                     query, created_at, top_k=8
                                 )
-                                docs_response = self.seach_docs(query, top_k=8)
+                                docs_response = seach_docs(query, top_k=8)
                                 docs_response.extend(embedding_results)
-                                internet_results = self.search_internet(query)
+                                internet_results = search_internet(query)
                                 docs_response.extend(internet_results)
                                 input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{docs_response}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls. Current conversation:"{chat_history_summary}"""
-                            elif not search_internet:
-                                embedding_results = self.search_pinecone(
+                            elif not search_online:
+                                embedding_results = search_pinecone(
                                     query, created_at, top_k=8
                                 )
-                                docs_response = self.seach_docs(query, top_k=8)
+                                docs_response = seach_docs(query, top_k=8)
                                 docs_response.extend(embedding_results)
                                 input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{docs_response}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls. Current conversation:"{chat_history_summary}"""
                     elif not search_docs:
-                        if search_internet:
-                            embedding_results = self.search_pinecone(
+                        if search_online:
+                            embedding_results = search_pinecone(
                                 query, created_at, top_k=16
                             )
-                            internet_results = self.search_internet(query)
+                            internet_results = search_internet(query)
                             embedding_results.extend(internet_results)
                             input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{embedding_results}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls. Current conversation:"{chat_history_summary}"""
-                        elif not search_internet:
-                            embedding_results = self.search_pinecone(
+                        elif not search_online:
+                            embedding_results = search_pinecone(
                                 query, created_at, top_k=16
                             )
                             input = f"""Provide a clear, well-organized, and critically analyzed respond to the following question of "{user_query}" in its original language, while leveraging the information of "{embedding_results}". Do not return any prefix like "AI:". Give corresponding detailed sources with urls. Current conversation:"{chat_history_summary}"""
 
                 with st.chat_message("assistant", avatar=ui.chat_ai_avatar):
                     st_cb = StreamHandler(st.empty())
-                    response = self.main_chain()(
+                    response = main_chain()(
                         {"input": input},
                         callbacks=[st_cb],
                     )
--- utils.py
+++ utils.py
@@ -1,20 +1,200 @@
 import os
 import tempfile
 import time
+import re
 from datetime import datetime
 
+import pinecone
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.openai_functions import create_structured_output_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.tools import DuckDuckGoSearchResults, WikipediaQueryRun
 from langchain.vectorstores import FAISS
+from langchain.utilities import WikipediaAPIWrapper
 from langchain.memory import XataChatMessageHistory
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain import LLMChain, PromptTemplate
+from langchain.schema import HumanMessage, SystemMessage
 from xata.client import XataClient
+from langchain.vectorstores import Pinecone
 
 import ui_config
 
 ui = ui_config.create_ui_from_config()
+llm_model = st.secrets["llm_model"]
+langchain_verbose = st.secrets["langchain_verbose"]
+
+def search_pinecone(query, created_at, top_k=16):
+    embeddings = OpenAIEmbeddings()
+    pinecone.init(
+        api_key=st.secrets["pinecone_api_key"],
+        environment=st.secrets["pinecone_environment"],
+    )
+    vectorstore = Pinecone.from_existing_index(
+        index_name=st.secrets["pinecone_index"],
+        embedding=embeddings,
+    )
+    if created_at is not None:
+        docs = vectorstore.similarity_search(
+            query, k=top_k, filter={"created_at": created_at}
+        )
+    else:
+        docs = vectorstore.similarity_search(query, k=top_k)
+
+    docs_list = []
+    for doc in docs:
+        date = datetime.fromtimestamp(doc.metadata["created_at"])
+        formatted_date = date.strftime("%Y-%m")  # Format date as 'YYYY-MM'
+        source_entry = "[{}. {}. {}.]({})".format(
+            doc.metadata["source_id"],
+            doc.metadata["author"],
+            formatted_date,
+            doc.metadata["url"],
+        )
+        docs_list.append({"content": doc.page_content, "source": source_entry})
+
+    return docs_list
+
+def search_internet(query):
+    search = DuckDuckGoSearchResults()
+    results = search.run(query)
+
+    pattern = r"\[snippet: (.*?), title: (.*?), link: (.*?)\]"
+    matches = re.findall(pattern, results)
+
+    docs = [
+        {"snippet": match[0], "title": match[1], "link": match[2]}
+        for match in matches
+    ]
+
+    docs_list = []
+
+    for doc in docs:
+        docs_list.append(
+            {
+                "content": doc["snippet"],
+                "source": "[{}]({})".format(doc["title"], doc["link"]),
+            }
+        )
+
+    return docs_list
+
+def search_wiki(query):
+    wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+    response_wiki = wikipedia.run(query)
+
+    return response_wiki
+
+def seach_docs(query, top_k=16):
+    docs = st.session_state["faiss_db"].similarity_search(query, k=top_k)
+    docs_list = []
+    for doc in docs:
+        source_entry = doc.metadata["source"]
+        docs_list.append({"content": doc.page_content, "source": source_entry})
+
+    return docs_list
+
+def func_calling_chain():
+    func_calling_json_schema = {
+        "title": "get_querys_and_filters_to_search_embeddings",
+        "description": "Extract the next queries and filters for a vector database semantic search from a chat history.",
+        "type": "object",
+        "properties": {
+            "query": {
+                "title": "Query",
+                "description": "The next queries extracted for a vector database semantic search from a chat history in the format of a JSON object",
+                "type": "string",
+            },
+            "created_at": {
+                "title": "Date Filters",
+                "description": 'Date extracted for a vector database semantic search from a chat history, in MongoDB\'s query and projection operators, in format like {"$gte": 1609459200.0, "$lte": 1640908800.0}',
+                "type": "string",
+            },
+            # "author": {
+            #     "title": "Author Filters",
+            #     "description": 'Author(s) extracted in MongoDB\'s query and projection operators, in format like {"$in": ["John Doe"]}',
+            #     "type": "string",
+            # },
+        },
+        "required": ["query"],
+    }
+
+    prompt_func_calling_msgs = [
+        SystemMessage(
+            content="You are a world class algorithm for extracting the next queries and filters for a vector database semantic search from a chat history. Make sure to answer in the correct structured format"
+        ),
+        HumanMessage(content="The chat history:"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+    ]
+
+    prompt_func_calling = ChatPromptTemplate(messages=prompt_func_calling_msgs)
+
+    llm_func_calling = ChatOpenAI(
+        model_name=llm_model, temperature=0, streaming=False
+    )
+
+    func_calling_chain = create_structured_output_chain(
+        output_schema=func_calling_json_schema,
+        llm=llm_func_calling,
+        prompt=prompt_func_calling,
+        verbose=langchain_verbose,
+    )
+
+    return func_calling_chain
+
+def chat_history_chain():
+    llm_chat_history = ChatOpenAI(
+        model=llm_model,
+        temperature=0,
+        streaming=False,
+        verbose=langchain_verbose,
+    )
+
+    template = """Return highly concise and well-organized chat history from: {input}"""
+    prompt = PromptTemplate(
+        input_variables=["input"],
+        template=template,
+    )
+
+    chat_history_chain = LLMChain(
+        llm=llm_chat_history,
+        prompt=prompt,
+        verbose=langchain_verbose,
+    )
+
+    return chat_history_chain
+
+def main_chain():
+    llm_chat = ChatOpenAI(
+        model=llm_model,
+        temperature=0,
+        streaming=True,
+        verbose=langchain_verbose,
+    )
+
+    template = """You MUST ONLY responese to science-related quests.
+    DO NOT return any information on politics, ethnicity, gender, national sovereignty, or other sensitive topics.
+    {input}
+    Use bullet points if a better expression effect can be achieved."""
+
+    prompt = PromptTemplate(
+        input_variables=["input"],
+        template=template,
+    )
+
+    chain = LLMChain(
+        llm=llm_chat,
+        prompt=prompt,
+        verbose=langchain_verbose,
+    )
+
+    return chain
+
+
 
 
 def xata_chat_history(_session_id: str):
