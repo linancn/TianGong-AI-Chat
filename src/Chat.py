16fc9979348d5b05dd7af95dd06f7612dd70a532
nanli <linanenv@gmail.com>
2023-09-25 12:51:19 +0800

    fix os.env

--- src/Chat.py
+++ src/Chat.py
@@ -1,20 +1,9 @@
 import datetime
 import json
-import os
 import time
 from datetime import datetime
 
 import streamlit as st
-
-os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
-os.environ["XATA_API_KEY"] = st.secrets["xata_api_key"]
-os.environ["XATA_DATABASE_URL"] = st.secrets["xata_db_url"]
-os.environ["LLM_MODEL"] = st.secrets["llm_model"]
-os.environ["LANGCHAIN_VERBOSE"] = str(st.secrets["langchain_verbose"])
-os.environ["PASSWORD"] = st.secrets["password"]
-os.environ["PINECONE_API_KEY"] = st.secrets["pinecone_api_key"]
-os.environ["PINECONE_ENVIRONMENT"] = st.secrets["pinecone_environment"]
-os.environ["PINECONE_INDEX"] = st.secrets["pinecone_index"]
 
 import ui_config
 import utils
