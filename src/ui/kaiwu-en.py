a2d23ca7cb6a649a318337c2baf0415e14cca9d9
KaiwuX <kaiwu_info@proton.me>
2023-10-23 09:16:20 +0000

    init kaiwu

---
+++ src/ui/kaiwu-en.py
@@ -0,0 +1,87 @@
+ui_data = {
+    "need_fixed_passwd": False,
+    "wix_login_title": """
+    <p align="center">
+        <img src="app/static/kaiwu/logo.png" alt="Kaiwu" width="60" style="margin-top: 50px"/>
+    </p>
+    <p align="center"  style="margin-bottom: 30px; font-size:24px">
+        Welcome to Kaiwu Chat
+    </p>
+    """,
+    "wix_login_username_label": "Email Address",
+    "wix_login_password_label": "Password",
+    "wix_login_button_label": "Log in",
+    "wix_signup_button_label": "Sign up on Kaiwu.info 🌐",
+    "wix_login_error_text": "Login failed, please check your username and password.",
+    "wix_signup_button_url": "https://www.kaiwu.info/",
+    "wix_login_error_icon": "🚨",
+    "theme": {
+        "primaryColor": "#1499E3",
+    },
+    "page_title": "Kaiwu Chat",
+    "page_icon": "src/static/kaiwu/favicon.ico",
+    "page_markdown": """
+      <style>
+          body {
+              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+          }
+          a {
+              text-decoration: none;
+          }
+          a:hover {
+              text-decoration: none;
+          }
+          div[data-testid="stDecoration"] {
+              background-image: linear-gradient(90deg, #1499E3, #E6FAFF);
+          }
+          div[data-testid="stImage"] {
+              min-width: 50px;
+              max-width: 50px;
+          }
+          section[data-testid="stSidebar"] {
+              min-width: 360px;
+              max-width: 480px;
+          }
+          .css-164nlkn {
+              text-align: right;
+          }
+          .text-sm {
+              font-size: .875rem;
+              line-height: 1.25rem
+          }
+      </style>
+      """,
+    "sidebar_image": "src/static/kaiwu/logo.png",
+    "sidebar_title": "Kaiwu Chat",
+    "sidebar_subheader": "Chat for sustainability",
+    "sidebar_welcome_text": """Hi, **{username}**! You're currently on the **{subscription}** plan.""",
+    "sidebar_markdown": """
+        <div style="position: fixed; left:25px; bottom: 10px; height:110px; width: 300px; font-size: 14px; font-weight: bold; background-color: #F0F2F6;">
+            <table style="border: 0; position: absolute; bottom: 5px;">
+                <tr style="border: 0;">
+                    <td style="border: 0; text-align: center; padding: 0;"><p class="text-sm" style="margin: 0; opacity: 0.5;"><a style="color: black" href="https://www.tiangong.world">Forked from TianGong AI 🧠</a></p></td>
+                </tr>
+            </table>
+        </div>
+        """,
+    "sidebar_expander_title": "Advanced Search Settings:",
+    "search_knowledge_base_checkbox_label": "Knowledge Base",
+    "search_internet_checkbox_label": "Internet",
+    "search_wikipedia_checkbox_label": "Wikipedia",
+    "search_arxiv_checkbox_label": "arXiv",
+    "search_docs_checkbox_label": "Documents",
+    "search_docs_options": "Options:",
+    "search_docs_options_isolated": "Isolated",
+    "search_docs_options_combined": "Combined",
+    "sidebar_file_uploader_title": "Documents to analyze:",
+    "sidebar_file_uploader_spinner": "Analyzing...",
+    "sidebar_file_uploader_error": "None of the files can be analyzed, please check the format!",
+    "sidebar_instructions": """""",
+    "current_chat_title": "Chat History :",
+    "chat_ai_avatar": "src/static/kaiwu/logo.png",
+    "chat_user_avatar": "src/static/kaiwu/user.png",
+    "chat_ai_welcome": "How can I help you?",
+    "chat_human_placeholder": "Ask for sustainability-related information, in any language you like!",
+    "sidebar_newchat_button_label": "New Chat",
+    "sidebar_delete_button_label": "Delete Chat",
+}
